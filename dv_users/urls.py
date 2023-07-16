add5a0d9da110ea14dbd6c107cff7b33f9dc6d5e
Sakhawy <50373064+sakhawy@users.noreply.github.com>
2023-07-16 06:38:03 +0300

    feat: add user creation

--- dv_users/urls.py
+++ dv_users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.urls import path
-from .views import DVUserTokenObtainPairView
+from .views import DVUserTokenObtainPairView, DVUserListCreateView
 
 urlpatterns = [
-    path('dv-user/token/', DVUserTokenObtainPairView.as_view(), name='dvuser-token-obtain-pair'),
+    path('dv_users/token/', DVUserTokenObtainPairView.as_view(), name='dvuser-token-obtain-pair'),
+    path('dv_users/', DVUserListCreateView.as_view(), name='dvuser-list-create'),
 ]
--- dv_users/views.py
+++ dv_users/views.py
@@ -1,5 +1,15 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
 from .tokens import DVUserTokenObtainPairSerializer
-
+from .models import DVUser
+from .serializers import DVUserSerializer
 class DVUserTokenObtainPairView(TokenObtainPairView):
     serializer_class = DVUserTokenObtainPairSerializer
+
+class DVUserListCreateView(generics.ListCreateAPIView):
+    queryset = DVUser.objects.all()
+    serializer_class = DVUserSerializer
+
+class DVUserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DVUser.objects.all()
+    serializer_class = DVUserSerializer
